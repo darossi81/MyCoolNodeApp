6c6d792dfae9bae6b82a2e79644fe826b445c995
Darren Rossiter <darren.rossiter@gmail.com>
2019-04-02 11:07:45 +0100

    creds removed

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 
 var config = {
-  consumer_key:         '6wNtD8keQwlmeCKymofmPKBz6',
-  consumer_secret:      'KhIjjNetsNrJUaN1tuGA7ogTMvBVKxlAK7e5E0L69mTiGlGgp7',
-  access_token:         '1096508026599673857-JoK7LZRoOL11q4PveXcaD5EdAglc2G',
-  access_token_secret:  'lTuv6DC8nF22VbyQw9f3yn3KNjJD0QN2fW1m9A4MNjVqO'
+  consumer_key:         '',
+  consumer_secret:      '',
+  access_token:         '',
+  access_token_secret:  ''
 }
 
 
