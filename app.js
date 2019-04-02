6cf719a08f37fbeb615130b74944ebc51f12d5ad
Darren Rossiter <darren.rossiter@gmail.com>
2019-04-02 16:18:31 +0100

    changing image dir again

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ function random_from_array(images){
 
 function upload_random_image(images){
   console.log('Opening an image...');
-  var image_path = path.join(__dirname - __dirname, 'http://exceltraining.london/draw/uploads/' + random_from_array(images)),
+  var image_path = path.join('', 'http://exceltraining.london/draw/uploads/' + random_from_array(images)),
       b64content = fs.readFileSync(image_path, { encoding: 'base64' });
 
   console.log('Uploading an image...');
@@ -64,7 +64,7 @@ function upload_random_image(images){
 }
 
 
-fs.readdir(__dirname - __dirname + 'http://exceltraining.london/draw/uploads/', function(err, files) {
+fs.readdir('' + 'http://exceltraining.london/draw/uploads/', function(err, files) {
   if (err){
     console.log(err);
   }
